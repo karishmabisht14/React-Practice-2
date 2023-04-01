ac45afd51b120bd7c18a082712ccbe386bcea68a
karishmabisht14 <karishma.qt14@gmail.com>
2023-04-01 16:15:13 +0530

    Using ReactDOM

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ const App = () => {
   };
 
   return (
-    <div>
+    <React.Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </React.Fragment>
   );
 };
 
